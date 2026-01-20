389ea0d6ef15d624d68ed63ae7c9024573d5a2a2
Adepetun22 <adepetun22@gmail.com>
2026-01-20 22:17:00 +0100

    Updates

--- src/components/DashboardCCC/MobileChat/ChatRoomMobile.jsx
+++ src/components/DashboardCCC/MobileChat/ChatRoomMobile.jsx
@@ -520,7 +520,7 @@ const ChatRoomMobile = ({ onBack, contactDetails = null }) => {
                 {/* Emoji Picker */}
                 {showEmojiPicker && (
                   <div className="absolute bottom-12 left-0">
-                    <EmojiPicker onEmojiSelect={handleEmojiSelect} marginLeft="-250px" />
+                    <EmojiPicker onEmojiSelect={handleEmojiSelect} marginLeft="-270px" />
                   </div>
                 )}
               </div>
